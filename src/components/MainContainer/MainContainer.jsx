51e1bdd224f884e23120b7f2f5c47b8b4db988a4
potato <Brittanycc@missionreadyhq.com>
2025-05-17 15:01:31 +1200

    Started creating the file upload button

--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
-import styles from "./MainContainer.module.css"
+import { React, useState } from "react";
+import styles from "./MainContainer.module.css";
 
 export default function MainContainer() {
-  return (
-    <div className={styles.maindivcontainer}>Why</div>
-  )
-}
+  //Variables
+    const [fileName, setFileName] = useState("");
+
+    const handleFileChange = (event) => {
+      if (event.target.files.length > 0) {
+        setFileName(event.target.files[0].name);
+      }
+    };
+
+    return (
+      <div className={styles.maindivcontainer}>
+        <h1 className={styles.title}>Get an insurance quote here</h1>
+        <div className={styles.imagediv}>
+          <input type="file" accept="image/*" onChange={handleFileChange} />
+        </div>
+      </div>
+    );
+  };
+
